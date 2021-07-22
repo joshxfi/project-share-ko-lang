37e36f9b3df8fbbbfb7429bc809e92dffc186c43
slify08 <islimaxmc@gmail.com>
2021-07-22 15:28:55 +0800

    updated server to async/await

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,17 +8,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (_, res: Response) => {
   res.send('Hello World!');
 });
 
 app.use('/api/posts', routes);
 
-mongoose.connect(process.env.MONGODB_URI ?? '', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ??
+    'mongodb+srv://joshxfi:Test08qq@xfidb.jdq15.mongodb.net/PSKL2?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
 mongoose.connection.on('connected', () => console.log('db is connected!'));
 
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -3,55 +3,66 @@ import Posts from '../models/posts';
 
 const router: Router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  Posts.find().then((data: object) => res.status(200).json(data));
+router.get('/', async (_, res: Response) => {
+  const data = await Posts.find();
+  res.status(200).json(data);
 });
 
-router.get('/:id', (req: Request, res: Response) => {
-  Posts.findById(req.params.id)
-    .then((data) =>
-      data
-        ? res.status(200).json(data)
-        : res
-            .status(404)
-            .json({ msg: 'Posts with the given ID could not be found!' })
-    )
-    .catch((err: string) => res.status(500).json({ error: err }));
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const data = await Posts.findById(req.params.id);
+
+    data
+      ? res.status(200).json(data)
+      : res
+          .status(404)
+          .json({ msg: 'Posts with the given ID could not be found!' });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   const newPost = new Posts(req.body);
-  newPost
-    .save()
-    .then((data: object) =>
-      res.status(201).json({
-        msg: 'Added post successfully!',
-        postAdded: data,
-      })
-    )
-    .catch((err: string) => res.status(500).json({ error: err }));
+
+  try {
+    const data = await newPost.save();
+    res.status(201).json({
+      msg: 'Added post successfully!',
+      postAdded: data,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
-router.patch('/:id', (req: Request, res: Response) => {
-  Posts.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
-    .then((data) =>
-      res.status(200).json({
-        msg: 'Post updated successfully',
-        postUpdated: data,
-      })
-    )
-    .catch((err: string) => res.status(500).json({ error: err }));
+router.patch('/:id', async (req: Request, res: Response) => {
+  try {
+    const data = await Posts.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(200).json({
+      msg: 'Post updated successfully',
+      postUpdated: data,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
-router.delete('/:id', (req: Request, res: Response) => {
-  Posts.deleteOne({ _id: req.params.id })
-    .then((data: object) =>
-      res.status(200).json({
-        msg: 'Post deleted successfully!',
-        deletedPost: data,
-      })
-    )
-    .catch((err: string) => res.status(500).json({ error: err }));
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const data = await Posts.deleteOne({ _id: req.params.id });
+    res.status(200).json({
+      msg: 'Post deleted successfully!',
+      deletedPost: data,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 export default router;
